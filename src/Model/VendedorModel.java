ce1a6af8dbbc7e74a4534b5c1e9d64dd3c289561
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-06-28 13:22:37 -0300

    Criada classe vendedor

---
+++ src/Model/VendedorModel.java
@@ -0,0 +1,33 @@
+package Model;
+
+public class VendedorModel {
+    private String nome;
+    private String email;
+    private String cpf;
+
+    public VendedorModel(String nome, String email, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String toString() {
+        return "\nNome do vendedor: " + getNome() +
+                "\nE-mail: " + getEmail() +
+                "\nCPF: " + getCpf();
+    }
+
+
+}
